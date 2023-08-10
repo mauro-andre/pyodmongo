d79ca827110d98c7f25bfd65b31b6d5d74a16166
Mauro André <eng.mauroandre@gmail.com>
2023-08-10 18:24:00 -0300

    prepare pypi

--- pyodmondo/__init__.py
+++ pyodmondo/__init__.py
@@ -3,8 +3,3 @@ from .engine.engine import DbEngine
 from .models.db_model import DbModel
 from .models.id_model import Id
 from .models.db_field import DbField
-# from .crud.save import save
-# from .crud.find import find_one, find_many
-# from .queries.comparison_operators import eq, gt, gte, in_, lt, lte, ne, nin
-# from .queries.query_string import mount_query_filter
-# from .queries.logical_operators import and_, or_
