64d9e82e8f69263bcb3e414d40b4503e28ae8f3b
Mauro André <eng.mauroandre@gmail.com>
2023-08-10 17:29:48 -0300

    added logical operators on init file

--- pyodmondo_pv2/queries/__init__.py
+++ pyodmondo_pv2/queries/__init__.py
@@ -1,2 +1,3 @@
-from .comparison_operators import eq, gt, gte, in_, lt, lte, ne, nin
+from .comparison_operators import eq, gt, gte, in_, lt, lte, ne, nin, text
+from .logical_operators import and_, or_
 from .query_string import mount_query_filter
