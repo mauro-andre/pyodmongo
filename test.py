c39eef545b9f1b91ac2e08abab6054adc30ae532
Mauro André <eng.mauroandre@gmail.com>
2023-08-23 15:38:19 -0300

    minimal pydantic _internal

--- test.py
+++ test.py
@@ -1,34 +1,16 @@
-from typing import Any, TYPE_CHECKING, Type, TypeVar, ClassVar, Annotated
-from pydantic_copy import BaseModel, Field
+from pyodmongo.pydantic_mod.main import BaseModel
 from datetime import datetime
-
-# class DbModelCore:
-#     @classmethod
-
-
-# class DbCore:
-#     @classmethod
-#     def __init_subclass__(cls):
-#         setattr(cls, 'id', 'VALOR DA VARIAVEL DE CLASSE')
 
 
 class DbModel(BaseModel):
-    id: str = Field(default=None)
+    id: str = None
     created_at: datetime = None
     updated_at: datetime = None
 
-    # @classmethod
-    # def __init_subclass__(cls):
-    #     for field in cls.model_fields:
-    #         setattr(cls, field, 'VALOR DA VARIAVEL DE CLASSE')
-
     @classmethod
     def __pydantic_init_subclass__(cls):
-        test: type[Annotated] = 'VRAU'
-        # setattr(cls, 'id', 'VALOR DA VARIAVEL DE CLASSE')
         for field in cls.model_fields:
             setattr(cls, field, 'VALOR DA VARIAVEL DE CLASSE')
-        print()
         pass
 
 
@@ -44,17 +26,6 @@ class Model2(Model1):
     attr2: str = 'VALOR DO OBJ'
 
 
-obj_2 = Model2(attr1='um')
+obj_2 = Model2()
 print(obj_2)
 print(Model2.attr2)
-
-# class Query:
-#     def __getattribute__(self, __name: str) -> Any:
-#         print(f'ATRIBUTO: {__name}')
-
-
-# def eq(Model: type[Model]) -> Model:
-#     return Query()
-
-
-# query = eq(Model2).attr2
