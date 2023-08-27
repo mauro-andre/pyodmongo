fc33ed3f044f557e938c486ee38f984695378594
Mauro André <eng.mauroandre@gmail.com>
2023-08-27 07:51:36 -0300

    test sync and async save

--- tests/test_async_crud_db.py
+++ tests/test_async_crud_db.py
@@ -1,9 +1,10 @@
-from pyodmongo import DbEngine, DbModel
+from pyodmongo import AsyncDbEngine, DbModel
 from typing import ClassVar
+import pytest
 
 mongo_uri = 'mongodb://localhost:27017'
 db_name = 'pyodmongo_pytest'
-db = DbEngine(mongo_uri=mongo_uri, db_name=db_name)
+db = AsyncDbEngine(mongo_uri=mongo_uri, db_name=db_name)
 
 
 class MyClass(DbModel):
@@ -15,10 +16,10 @@ class MyClass(DbModel):
 # {'n': 1, 'nModified': 1, 'ok': 1.0, 'updatedExisting': True}
 # {'n': 1, 'upserted': ObjectId('64ead6318257a081b9709d59'), 'nModified': 0, 'ok': 1.0, 'updatedExisting': False}
 
-
-def test_check_if_create_a_new_doc_on_save():
+@pytest.mark.asyncio
+async def test_check_if_create_a_new_doc_on_save():
     obj = MyClass(attr1='attr_1', attr2='attr_2')
     db._db[MyClass._collection].drop()
-    result = db.save(obj)
+    result = await db.save(obj)
     assert result.get('upserted') is not None
     db._db[MyClass._collection].drop()
