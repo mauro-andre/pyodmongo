fce9febef20418bfd08c1652ce2303d6fccaef47
Mauro André <eng.mauroandre@gmail.com>
2023-08-27 13:12:48 -0300

    refactor mount query string

--- pyodmongo/queries/query_string.py
+++ pyodmongo/queries/query_string.py
@@ -1,13 +1,14 @@
+from ..models.db_model import DbModel
 from ..models.db_field_info import DbFieldInfo
+from ..models.query_operators import LogicalOperator, ComparisonOperator
 from .comparison_operators import comparison_operator
 from .logical_operators import and_
+from typing import Type
 
 
-def get_field_info(Model, field_name):
-    return eval('Model.' + field_name)
-
-
-def mount_query_filter(items: dict, Model, operators: list):
+def mount_query_filter(Model: Type[DbModel], items: dict, initial_comparison_operators: list[ComparisonOperator]) -> LogicalOperator:
+    if DbModel not in Model.__bases__:
+        raise TypeError('Model must be a DbModel')
     for key, value in items.items():
         value = value.strip()
         if value == '':
@@ -22,10 +23,10 @@ def mount_query_filter(items: dict, Model, operators: list):
             value = value
         field_name = split_result[0]
         try:
-            field_info: DbFieldInfo = get_field_info(Model=Model, field_name=field_name)
+            db_field_info: DbFieldInfo = getattr(Model, field_name)
         except AttributeError:
-            raise AttributeError(f"There's no field '{field_name}'")
-        operators.append(comparison_operator(field_info=field_info, operator=operator, value=value))
-    if len(operators) == 0:
+            raise AttributeError(f"There's no field '{field_name}' in {Model.__name__}")
+        initial_comparison_operators.append(comparison_operator(field_info=db_field_info, operator=operator, value=value))
+    if len(initial_comparison_operators) == 0:
         return {}
-    return and_(*operators)
+    return and_(*initial_comparison_operators)
