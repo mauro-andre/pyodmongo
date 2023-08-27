91a79266ab94b875155d13e197a421f745ec21b1
Mauro André <eng.mauroandre@gmail.com>
2023-08-27 12:02:06 -0300

    export ResposePaginate

--- pyodmongo/__init__.py
+++ pyodmongo/__init__.py
@@ -2,4 +2,5 @@ from .async_engine.engine import AsyncDbEngine
 from .engine.engine import DbEngine
 from .models.db_model import DbModel
 from .models.id_model import Id
+from .models.paginate import ResponsePaginate
 from .pydantic_mod.fields import Field
