61de7f5791e12878d49d7b6b9f885bb35306e9e7
Mauro André <eng.mauroandre@gmail.com>
2023-09-09 16:27:39 -0300

    update tests crud

--- tests/test_crud_db.py
+++ tests/test_crud_db.py
@@ -1,6 +1,7 @@
-from pyodmongo import DbEngine, DbModel
+from pyodmongo import DbEngine, DbModel, SaveResponse, DeleteResponse
 from pyodmongo.queries import eq
 from typing import ClassVar
+from bson import ObjectId
 import pytest
 
 mongo_uri = 'mongodb://localhost:27017'
@@ -14,27 +15,75 @@ class MyClass(DbModel):
     _collection: ClassVar = 'my_class_test'
 
 
-# {'n': 1, 'nModified': 1, 'ok': 1.0, 'updatedExisting': True}
-# {'n': 1, 'upserted': ObjectId('64ead6318257a081b9709d59'), 'nModified': 0, 'ok': 1.0, 'updatedExisting': False}
-
 @pytest.fixture()
 def drop_collection():
     db._db[MyClass._collection].drop()
-    yield
+    yield MyClass(attr1='attr_1', attr2='attr_2')
     db._db[MyClass._collection].drop()
 
 
-def test_check_if_create_a_new_doc_on_save(drop_collection):
-    obj = MyClass(attr1='attr_1', attr2='attr_2')
-    result = db.save(obj)
-    assert result.get('upserted') is not None
+@pytest.fixture()
+def new_obj() -> type[MyClass]:
+    yield MyClass(attr1='attr_1', attr2='attr_2')
 
 
-def test_create_and_delete_one(drop_collection):
-    obj = MyClass(attr1='attr_1', attr2='attr_2')
-    result = db.save(obj)
-    assert result.get('upserted') is not None
-    id = result.get('upserted')
+def test_check_if_create_a_new_doc_on_save(drop_collection, new_obj):
+    result: SaveResponse = db.save(new_obj)
+    assert ObjectId.is_valid(result.upserted_id)
+
+
+def test_create_and_delete_one(drop_collection, new_obj):
+    result: SaveResponse = db.save(new_obj)
+    assert result.upserted_id is not None
+    id = result.upserted_id
     query = eq(MyClass.id, id)
-    result = db.delete_one(Model=MyClass, query=query)
-    assert result.get('document_deleted') == 1
+    result: DeleteResponse = db.delete_one(Model=MyClass, query=query)
+    assert result.deleted_count == 1
+
+
+def test_find_one(drop_collection, new_obj):
+    result: SaveResponse = db.save(new_obj)
+    id_returned = result.upserted_id
+    obj_found = db.find_one(MyClass, eq(MyClass.id, id_returned))
+
+    assert isinstance(obj_found, MyClass)
+    assert obj_found.id == id_returned
+
+
+@pytest.fixture()
+def objs() -> list[SaveResponse]:
+    objs = [
+        MyClass(attr1='attr_1', attr2='attr_2'),
+        MyClass(attr1='attr_1', attr2='attr_2'),
+        MyClass(attr1='attr_1', attr2='attr_2'),
+        MyClass(attr1='attr_3', attr2='attr_4'),
+        MyClass(attr1='attr_3', attr2='attr_4'),
+        MyClass(attr1='attr_5', attr2='attr_6'),
+    ]
+    return objs
+
+
+def test_save_all_created(drop_collection, objs):
+    response: list[SaveResponse] = db.save_all(objs)
+    upseted_quantity = 0
+    for obj_response in response:
+        obj_response: SaveResponse
+        upseted_quantity += 1 if ObjectId.is_valid(obj_response.upserted_id) else 0
+
+    assert upseted_quantity == 6
+
+
+def test_update_on_save(drop_collection, objs):
+    db.save_all(objs)
+    obj = MyClass(attr1='value_1', attr2='value_2')
+    response: SaveResponse = db.save(obj, eq(MyClass.attr1, 'attr_3'))
+
+    assert response.matched_count == 2
+    assert response.modified_count == 2
+    assert response.upserted_id is None
+
+
+def test_delete(drop_collection, objs):
+    db.save_all(objs)
+    response: DeleteResponse = db.delete(MyClass, eq(MyClass.attr1, 'attr_1'))
+    assert response.deleted_count == 3
