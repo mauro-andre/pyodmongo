fb5415a727cf2a207de85528e157c4dc270e0e8f
Mauro André <eng.mauroandre@gmail.com>
2023-09-15 06:07:16 -0300

    ✅ indexes in inheritance

--- tests/test_db_index.py
+++ tests/test_db_index.py
@@ -3,7 +3,7 @@ from pymongo import IndexModel
 from typing import ClassVar
 import pytest
 import pytest_asyncio
-from bson import SON
+from pprint import pprint
 
 
 mongo_uri = 'mongodb://localhost:27017'
@@ -18,21 +18,21 @@ class MyClass(DbModel):
     attr_2: str = Field(unique=True)
     attr_3: str = Field(text_index=True)
     attr_4: str = Field(index=True, text_index=True)
-    _collection: ClassVar = 'myclass'
+    _collection: ClassVar = 'my_class'
 
 
-# @pytest.fixture()
-# def sync_drop_collection():
-#     sync_db._db[MyClass._collection].drop()
-#     yield
-#     sync_db._db[MyClass._collection].drop()
+@pytest.fixture()
+def sync_drop_collection():
+    sync_db._db[MyClass._collection].drop()
+    yield
+    sync_db._db[MyClass._collection].drop()
 
 
-# @pytest_asyncio.fixture()
-# async def async_drop_collection():
-#     await async_db._db[MyClass._collection].drop()
-#     yield
-#     await async_db._db[MyClass._collection].drop()
+@pytest_asyncio.fixture()
+async def async_drop_collection():
+    await async_db._db[MyClass._collection].drop()
+    yield
+    await async_db._db[MyClass._collection].drop()
 
 
 def test_index_unique_text_index():
@@ -58,15 +58,56 @@ def test_index_unique_text_index():
 
 
 def test_check_index_field():
-    assert hasattr(MyClass, '_indexes')
-    assert len(MyClass._indexes) == 3
-    assert all(type(item) is IndexModel for item in MyClass._indexes)
+    assert hasattr(MyClass, '_init_indexes')
+    assert len(MyClass._init_indexes) == 3
+    assert all(type(item) is IndexModel for item in MyClass._init_indexes)
 
 
-# def test_check_index_ind_db(sync_drop_collection):
-#     obj = MyClass(attr_0='attr_0', attr_1='attr_1', attr_2='attr_2', attr_3='attr_3', attr_4='attr_4')
-#     result = sync_db.save(obj)
-#     indexes_in_db = sync_db._db[MyClass._collection].list_indexes()
-#     for index in indexes_in_db:
-#         index: SON
-#         print(index['attr1'])
+def test_sync_check_index_in_db(sync_drop_collection):
+    obj = MyClass(attr_0='attr_0', attr_1='attr_1', attr_2='attr_2', attr_3='attr_3', attr_4='attr_4')
+    result = sync_db.save(obj)
+    indexes_in_db = sync_db._db[MyClass._collection].index_information()
+    assert 'attr_0' not in indexes_in_db
+    assert 'attr_1' in indexes_in_db
+    assert 'attr_2' not in indexes_in_db
+    assert 'attr_3' not in indexes_in_db
+    assert 'attr_4' in indexes_in_db
+    assert 'attr_3' in indexes_in_db['texts']['weights']
+    assert 'attr_4' in indexes_in_db['texts']['weights']
+
+
+@pytest.mark.asyncio
+async def test_async_check_index_in_db(async_drop_collection):
+    obj = MyClass(attr_0='attr_0', attr_1='attr_1', attr_2='attr_2', attr_3='attr_3', attr_4='attr_4')
+    result = await async_db.save(obj)
+    indexes_in_db = await async_db._db[MyClass._collection].index_information()
+    assert 'attr_0' not in indexes_in_db
+    assert 'attr_1' in indexes_in_db
+    assert 'attr_2' not in indexes_in_db
+    assert 'attr_3' not in indexes_in_db
+    assert 'attr_4' in indexes_in_db
+    assert 'attr_3' in indexes_in_db['texts']['weights']
+    assert 'attr_4' in indexes_in_db['texts']['weights']
+
+
+def test_create_indexes_on_inheritance(sync_drop_collection):
+    class InheritanceClass(MyClass):
+        attr_5: str = Field(index=True)
+
+    obj = InheritanceClass(attr_0='attr_0',
+                           attr_1='attr_1',
+                           attr_2='attr_2',
+                           attr_3='attr_3',
+                           attr_4='attr_4',
+                           attr_5='attr_5')
+
+    result = sync_db.save(obj)
+    indexes_in_db = sync_db._db[MyClass._collection].index_information()
+    assert 'attr_0' not in indexes_in_db
+    assert 'attr_1' in indexes_in_db
+    assert 'attr_2' not in indexes_in_db
+    assert 'attr_3' not in indexes_in_db
+    assert 'attr_4' in indexes_in_db
+    assert 'attr_5' in indexes_in_db
+    assert 'attr_3' in indexes_in_db['texts']['weights']
+    assert 'attr_4' in indexes_in_db['texts']['weights']
