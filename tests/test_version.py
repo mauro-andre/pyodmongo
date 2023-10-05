3c2315abc229fe78429d7a5903e60df06d25617d
Mauro André <eng.mauroandre@gmail.com>
2023-10-05 12:09:55 -0300

    ✅ Test version

---
+++ tests/test_version.py
@@ -0,0 +1,17 @@
+import platform
+import sys
+import pymongo
+import motor
+from pyodmongo import version
+
+
+def test_version():
+    info = {
+        "PyODMongo version": "__VERSION__",
+        "Pymongo version": pymongo.__version__,
+        "Motor version": motor._version.version,
+        "Python version": sys.version,
+        "Platform": platform.platform(),
+    }
+    response_expected = "\n".join([f"{key}: {value}" for key, value in info.items()])
+    assert response_expected == version.response
