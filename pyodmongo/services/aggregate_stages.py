d7e971a53734cd9baaed070dd302c3e61f29ab31
Mauro Andre <eng.mauroandre@gmail.com>
2024-04-09 23:23:42 -0300

    🔥 remove lookuo_and_set single function

--- pyodmongo/services/aggregate_stages.py
+++ pyodmongo/services/aggregate_stages.py
@@ -1,28 +1,3 @@
-def lookup_and_set(
-    from_: str,
-    local_field: str,
-    foreign_field: str,
-    as_: str,
-    pipeline: list,
-    is_reference_list: bool,
-):
-    lookup_stage = [
-        {
-            "$lookup": {
-                "from": from_,
-                "localField": local_field,
-                "foreignField": foreign_field,
-                "as": as_,
-                "pipeline": pipeline,
-            }
-        }
-    ]
-    set_stage = [{"$set": {as_: {"$arrayElemAt": [f"${as_}", 0]}}}]
-    if is_reference_list:
-        return lookup_stage
-    return lookup_stage + set_stage
-
-
 def unwind(path: str, array_index: str, preserve_empty: bool):
     return [
         {
