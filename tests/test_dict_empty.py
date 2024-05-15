5b9de319eeef627c3e4d3e03746049e95c1a45ec
Mauro Andre <eng.mauroandre@gmail.com>
2024-05-15 09:44:41 -0300

    🐛 Fix DbModel init not set None in empty list

--- tests/test_dict_empty.py
+++ tests/test_dict_empty.py
@@ -8,6 +8,7 @@ def test_empty_dict():
 
     class MyModel2(DbModel):
         attr2: str | None
+        attr2_list: list | None = []
         my_model_1: MyModel1 | Id | None
         _collection = "my_model_2"
 
@@ -16,13 +17,19 @@ def test_empty_dict():
         my_model_2: MyModel2 | Id | None
         my_model_2_2: MyModel2 | Id | None
         my_model_2_3: MyModel2 | Id | None
+        my_model_2_4: MyModel2 | Id | None
         _collection = "my_model_3"
 
     dct = {
         "attr3": "attr3",
-        "my_model_2": {"attr2": "Escrito", "my_model_1": {"attr1": {}}},
-        "my_model_2_2": {"my_model_1": {}},
+        "my_model_2": {
+            "attr2": "Escrito",
+            "attr2_list": [],
+            "my_model_1": {"attr1": {}},
+        },
+        "my_model_2_2": {"my_model_1": {}, "attr2_list": None},
         "my_model_2_3": {},
+        "my_model_2_4": {"attr2": None, "attr2_list": [], "my_model_1": {}},
     }
     obj = MyModel3(**dct)
     assert obj.model_dump() == {
@@ -32,10 +39,19 @@ def test_empty_dict():
             "created_at": None,
             "updated_at": None,
             "attr2": "Escrito",
+            "attr2_list": [],
             "my_model_1": None,
         },
         "my_model_2_2": None,
         "my_model_2_3": None,
+        "my_model_2_4": {
+            "id": None,
+            "created_at": None,
+            "updated_at": None,
+            "attr2": None,
+            "attr2_list": [],
+            "my_model_1": None,
+        },
         "id": None,
         "created_at": None,
         "updated_at": None,
